3601bbe426b84563a033919431e1027147daf700
priantikonap <priantiko@gmail.com>
2019-12-31 17:21:29 +0700

    ganti function

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	router := gin.Default()
+	router.GET("/", getHome)
+
+	router.Run()
+}
+
+func getHome(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Berhasil di Home",
 	})
 }
